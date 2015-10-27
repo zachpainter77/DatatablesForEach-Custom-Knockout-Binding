8ec51d4f291ba90c76aa1e2dd2361df9cb955b5c
Zach Painter <zachpainter77@gmail.com>
2015-10-27 11:57:03 -0700

    Major Refactor
    I updated the binding to use the knockout subscriptions instead of the beforeRenderAll event callback.  When removing array items, the datatables were being destroyed too late causing the bindings and knockout to act weird. I suspect this is because of some race conditions that were happening... anyways, switching to the subscribe method fixed this.

--- DatatablesForEach.js
+++ DatatablesForEach.js
@@ -1,35 +1,39 @@
 ko.bindingHandlers.DataTablesForEach = {
-            
+            page: 0,
             init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-                 var nodes = Array.prototype.slice.call(element.childNodes, 0);
-                ko.utils.arrayForEach(nodes, function (node) {
-                    if (node && node.nodeType !== 1) {
-                        node.parentNode.removeChild(node);
-                    }
-                });
+                        valueAccessor().data.subscribe(function (changes) {
+                                    var table = $(element).closest('table').DataTable();
+                                    ko.bindingHandlers.DataTablesForEach.page = table.page();
+                                    table.destroy();
+                        }, null, 'arrayChange');            
+                        var nodes = Array.prototype.slice.call(element.childNodes, 0);
+                        ko.utils.arrayForEach(nodes, function (node) {
+                                    if (node && node.nodeType !== 1) {
+                                                node.parentNode.removeChild(node);
+                                    }
+                        });
                 return ko.bindingHandlers.foreach.init(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext);
             },
             update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-
-                var value = ko.unwrap(valueAccessor()),
-                key = "DataTablesForEach_Initialized";
-
-                var newValue = function () {
-                    return {
-                        data: value.data || value,
-                        beforeRenderAll: function (el, index, data) {
-
-                            if (ko.utils.domData.get(element, key)) {
-                                $(element).closest('table').DataTable().destroy();
-                            }
-                        },
-                        afterRenderAll: function (el, index, data) {
-                            $(element).closest('table').DataTable(value.options);
-                        }
-
-                    };
+                        var value = ko.unwrap(valueAccessor()),
+                        key = 'DataTablesForEach_Initialized';
+                        var newValue = function () {
+                                    return {
+                                                data: value.data || value,
+                                                afterRenderAll: function () {
+                                                            var table = $(element).closest('table').DataTable(value.options);
+                                                            if (value.options.paging) {
+                                                                        if (table.page.info().pages - ko.bindingHandlers.DataTablesForEach.page == 0) {
+                                                                                    table.page(--ko.bindingHandlers.DataTablesForEach.page).draw(false);
+                                                                        }
+                                                            else {
+                                                                        table.page(ko.bindingHandlers.DataTablesForEach.page).draw(false);
+                                                            }
+                                                }
+                                    }
+                        };
                 };
-
+                
                 ko.bindingHandlers.foreach.update(element, newValue, allBindingsAccessor, viewModel, bindingContext);
 
                 //if we have not previously marked this as initialized and there is currently items in the array, then cache on the element that it has been initialized
@@ -39,4 +43,4 @@ ko.bindingHandlers.DataTablesForEach = {
 
                 return { controlsDescendantBindings: true };
             }
-        };
+}; 
