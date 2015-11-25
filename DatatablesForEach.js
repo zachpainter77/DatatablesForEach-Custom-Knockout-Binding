28409b907e1a68c7ba9b1978e298702404440981
Zach Painter <zachpainter77@gmail.com>
2015-11-25 12:55:27 -0800

    Update Binding to be non-dependent on custom version of knockout
    Updated the binding to work without the need for a custom version of knockout! This binding now works with vanilla knockout 3.4!

--- DatatablesForEach.js
+++ DatatablesForEach.js
@@ -1,46 +1,37 @@
-ko.bindingHandlers.DataTablesForEach = {
-            page: 0,
-            init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-                        valueAccessor().data.subscribe(function (changes) {
-                                    var table = $(element).closest('table').DataTable();
-                                    ko.bindingHandlers.DataTablesForEach.page = table.page();
-                                    table.destroy();
-                        }, null, 'arrayChange');            
-                        var nodes = Array.prototype.slice.call(element.childNodes, 0);
-                        ko.utils.arrayForEach(nodes, function (node) {
-                                    if (node && node.nodeType !== 1) {
-                                                node.parentNode.removeChild(node);
-                                    }
-                        });
-                return ko.bindingHandlers.foreach.init(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext);
-            },
-            update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-                        var value = ko.unwrap(valueAccessor()),
-                        key = 'DataTablesForEach_Initialized';
-                        var newValue = function () {
-                                    return {
-                                                data: value.data || value,
-                                                afterRenderAll: function () {
-                                                            var table = $(element).closest('table').DataTable(value.options);
-                                                            if (value.options.paging) {
-                                                                        if (table.page.info().pages - ko.bindingHandlers.DataTablesForEach.page == 0) {
-                                                                                    table.page(--ko.bindingHandlers.DataTablesForEach.page).draw(false);
-                                                                        }
-                                                            else {
-                                                                        table.page(ko.bindingHandlers.DataTablesForEach.page).draw(false);
-                                                            }
-                                                }
-                                    }
-                        };
-                };
-                
-                ko.bindingHandlers.foreach.update(element, newValue, allBindingsAccessor, viewModel, bindingContext);
-
-                //if we have not previously marked this as initialized and there is currently items in the array, then cache on the element that it has been initialized
-                if (!ko.utils.domData.get(element, key) && (value.data || value.length)) {
-                    ko.utils.domData.set(element, key, true);
-                }
-
-                return { controlsDescendantBindings: true };
+ko.bindingHandlers.dataTablesForEach = {
+    page: 0,
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        
+    	valueAccessor().data.subscribe(function (changes) {
+        	var table = $(element).closest('table').DataTable();
+            ko.bindingHandlers.dataTablesForEach.page = table.page();
+            table.destroy();
+        }, null, 'arrayChange');            
+        var nodes = Array.prototype.slice.call(element.childNodes, 0);
+        ko.utils.arrayForEach(nodes, function (node) {
+        	if (node && node.nodeType !== 1) {
+            	node.parentNode.removeChild(node);	
             }
+        });
+        return ko.bindingHandlers.foreach.init(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext);
+    },
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {        
+        var options = ko.unwrap(valueAccessor()),
+            key = 'DataTablesForEach_Initialized';
+        ko.unwrap(options.data);       
+        ko.bindingHandlers.foreach.update(element, valueAccessor, allBindings, viewModel, bindingContext);
+        (function() {
+            console.log(options);
+            var table = $(element).closest('table').DataTable(options.dataTableOptions);
+            if (options.dataTableOptions.paging) {
+                if (table.page.info().pages - ko.bindingHandlers.dataTablesForEach.page == 0) 
+                    table.page(--ko.bindingHandlers.dataTablesForEach.page).draw(false);                
+                else 
+                    table.page(ko.bindingHandlers.dataTablesForEach.page).draw(false);                
+            }
+        })();
+        if (!ko.utils.domData.get(element, key) && (options.data || options.length))
+            ko.utils.domData.set(element, key, true);
+        return { controlsDescendantBindings: true };
+    }
 }; 
