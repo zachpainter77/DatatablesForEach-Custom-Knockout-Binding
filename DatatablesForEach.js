df8e128b112b036285ebc2b99e7881f528f40a35
Zach Painter <zachpainter77@gmail.com>
2015-12-03 12:51:53 -0800

    Refactored Binding
    Refactored

--- DatatablesForEach.js
+++ DatatablesForEach.js
@@ -1,12 +1,15 @@
 ko.bindingHandlers.dataTablesForEach = {
-    page: 0,
+	page: 0,
     init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-        
+      var options = ko.unwrap(valueAccessor());
+      ko.unwrap(options.data);
+      if(options.dataTableOptions.paging){
     	valueAccessor().data.subscribe(function (changes) {
         	var table = $(element).closest('table').DataTable();
             ko.bindingHandlers.dataTablesForEach.page = table.page();
             table.destroy();
-        }, null, 'arrayChange');            
+        }, null, 'arrayChange');          
+      }
         var nodes = Array.prototype.slice.call(element.childNodes, 0);
         ko.utils.arrayForEach(nodes, function (node) {
         	if (node && node.nodeType !== 1) {
@@ -18,17 +21,20 @@ ko.bindingHandlers.dataTablesForEach = {
     update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {        
         var options = ko.unwrap(valueAccessor()),
             key = 'DataTablesForEach_Initialized';
-        ko.unwrap(options.data);       
+        ko.unwrap(options.data);
+        var table;
+        if(!options.dataTableOptions.paging){
+          table = $(element).closest('table').DataTable();
+        	table.destroy();
+        }
         ko.bindingHandlers.foreach.update(element, valueAccessor, allBindings, viewModel, bindingContext);
-        (function() {
-            var table = $(element).closest('table').DataTable(options.dataTableOptions);
-            if (options.dataTableOptions.paging) {
-                if (table.page.info().pages - ko.bindingHandlers.dataTablesForEach.page == 0) 
-                    table.page(--ko.bindingHandlers.dataTablesForEach.page).draw(false);                
-                else 
-                    table.page(ko.bindingHandlers.dataTablesForEach.page).draw(false);                
-            }
-        })();
+        table = $(element).closest('table').DataTable(options.dataTableOptions);
+        if (options.dataTableOptions.paging) {
+           if (table.page.info().pages - ko.bindingHandlers.dataTablesForEach.page == 0) 
+               table.page(--ko.bindingHandlers.dataTablesForEach.page).draw(false);                
+           else 
+               table.page(ko.bindingHandlers.dataTablesForEach.page).draw(false);                
+        }        
         if (!ko.utils.domData.get(element, key) && (options.data || options.length))
             ko.utils.domData.set(element, key, true);
         return { controlsDescendantBindings: true };
